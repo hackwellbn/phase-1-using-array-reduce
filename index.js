b8da1fea2b8104078111f43aa2690fd03398f2a4
hackwellbn <119762901+hackwellbn@users.noreply.github.com>
2025-03-19 15:02:07 +0000

    adds reduce

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
 // Code your solution here
+
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5]; // Example battery batches
+
+const totalBatteries = batteryBatches.reduce((total, currentBatch) => total + currentBatch, 0);
+
+console.log(totalBatteries); // Output will be the sum of the battery amounts
+
